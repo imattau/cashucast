20a4a75714b6532bfeb2defa3544452f87daf3da
imattau <imatt.au@protonmail.com>
2025-08-03 20:34:05 +1000

    fix: import ssb-browser-core with named export

--- packages/ambient.d.ts
+++ packages/ambient.d.ts
@@ -1,4 +1,10 @@
-declare module 'ssb-browser-core/net';
+declare module 'ssb-browser-core/net' {
+  export function init(
+    dir: string,
+    overwriteConfig?: any,
+    extraModules?: any,
+  ): any;
+}
 declare module 'ssb-blobs';
 declare module 'webtorrent';
 declare module 'bittorrent-dht';
