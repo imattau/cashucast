5f17e4df3b045e48bb8262ff43f8dafa9e8ce113
imattau <imatt.au@protonmail.com>
2025-08-03 19:32:26 +1000

    polyfill Buffer in web entry

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Buffer } from "buffer";
 import App from "./App";
+
+globalThis.Buffer = globalThis.Buffer || Buffer;
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
