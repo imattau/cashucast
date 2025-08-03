36711a0c5ed4f0d05ec3238c42b63a4996129a09
imattau <imatt.au@protonmail.com>
2025-08-03 19:05:26 +1000

    fix: allow ssb-browser-core bundling

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['ssb-blobs', 'ssb-browser-core/net'],
+    exclude: ['ssb-blobs'],
   },
 });
