b29bbb26ca63b291ba1b2663a3d6f3064ce6f2fa
imattau <imatt.au@protonmail.com>
2025-08-04 18:06:24 +1000

    chore(web): remove unused Onboarding import from router

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -3,7 +3,6 @@
  * React component for router.
  */
 import React from 'react';
-import Onboarding from './routes/Onboarding';
 import Compose from './routes/Compose';
 import SettingsOverlay from './routes/SettingsOverlay';
 import SearchResults from './routes/SearchResults';
